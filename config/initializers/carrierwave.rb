04f65371e46e9f0f6c0acb42d7d142e5466fe995
GSchwikk <gschwikk@gmail.com>
2016-01-15 09:48:33 +0200

    Changed carrierwave to precompile on command only

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,12 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV["aws_access_key_id"],                        # required
-    :aws_secret_access_key  => ENV["aws_secret_access_key"], 
-    :region 				=> 'us-east-1'                        # required
-  }
-  config.fog_directory  = ENV["fog_directory"]                    # required
+  unless ENV['AWS_ACCESS_KEY_ID'].blank?
+	  config.fog_provider = 'fog/aws'
+	  config.fog_credentials = {
+	    :provider               => 'AWS',                        # required
+	    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],                        # required
+	    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"], 
+	    :region 				=> 'us-east-1'                        # required
+	  }
+	  config.fog_directory  = ENV["fog_directory"]                    # required
+	end
 end
