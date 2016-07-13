210f3d106194d9bc039e66b56308f10a81280194
GSchwikk <gschwikk@gmail.com>
2016-07-13 14:20:26 +0200

    Add 4 week variance and status

--- app/helpers/packs_helper.rb
+++ app/helpers/packs_helper.rb
@@ -1,15 +1,23 @@
 module PacksHelper
     
     def status_icon(kpi,var)
-        if kpi.vector == "Maximise" && var >= 0
-            "<td class='green'> <span class='glyphicon glyphicon-ok'></span> </td>".html_safe
-        elsif kpi.vector == "Maximise" && var < 0
-            "<td class='red'> <span class='glyphicon glyphicon-remove'></span> </td>".html_safe
-        elsif kpi.vector == "Minimise" && var <= 0
-            "<td class='green'> <span class='glyphicon glyphicon-ok'></span> </td>".html_safe
-        elsif kpi.vector == "Minimise" && var > 0
-            "<td class='red'> <span class='glyphicon glyphicon-remove'></span> </td>".html_safe
-        end   
+        if var 
+            if kpi.vector == "Maximise" && var >= 0
+                "<td class='green'> <span class='glyphicon glyphicon-ok'></span> </td>".html_safe
+            elsif kpi.vector == "Maximise" && var < 0
+                "<td class='red'> <span class='glyphicon glyphicon-remove'></span> </td>".html_safe
+            elsif kpi.vector == "Minimise" && var <= 0
+                "<td class='green'> <span class='glyphicon glyphicon-ok'></span> </td>".html_safe
+            elsif kpi.vector == "Minimise" && var > 0
+                "<td class='red'> <span class='glyphicon glyphicon-remove'></span> </td>".html_safe
+            end   
+        else
+            nil
+        end
+    end
+
+    def units_convert(string)
+        string == "Number" ? "#" : "%"
     end
 
 end
